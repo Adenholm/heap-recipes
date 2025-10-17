3431ac49641773fd5bb081107bcc4d1e13e69eff
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-10-17 15:03:11 +0200

    added nginx proxy

--- client/src/service/apiClient.tsx
+++ client/src/service/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5036/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api/recipes',
 });
 
 // Request interceptor to add JWT token
