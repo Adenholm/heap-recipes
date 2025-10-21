902264a1d42e8e9610456b7aecd06855a533669d
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-10-21 11:10:43 +0200

    Fixed the css for the mobile recipe detail view

--- client/src/pages/recipepage/Recipepage.tsx
+++ client/src/pages/recipepage/Recipepage.tsx
@@ -18,7 +18,19 @@ const RecipePage = () => {
     const { id } = useParams<{ id: string }>();
     const { openModal, setModal } = useContext(ModalContext);
     const [recipe, setRecipe] = useState<Recipe | null>(null);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     useEffect(() => {
         const fetchRecipe = async () => {
@@ -46,10 +58,30 @@ const RecipePage = () => {
         openModal();
     };
 
+    const Header = () => (
+        <div>
+            <div className="recipe-header">
+                <h1>{recipe.title}</h1>
+            </div>
+            
+            <div className="recipe-details">
+                <p>{recipe.prepTime} min</p>
+                <p>|</p>
+                <p>{recipe.servings} portioner</p>
+                <p>|</p>
+                <p>{recipe.tags.map(tag => tag.name).join(", ")}</p>
+            </div>
+            <p className="recipe-description">{recipe.description}</p>
+        </div>
+    );
+    
     return (
         <div className="recipe-page">
 
             <div className="recipe-container">
+                {isAuthenticated && <img src={editIcon} alt="Edit Recipe" className='edit-buttons' onClick={onEdit}/>}
+                {isAuthenticated && <img src={deleteIcon} alt="Delete Recipe" className='edit-buttons delete-button' onClick={showDeleteModal}/>}
+                {isMobile && <Header />}
                 <aside>
                     <img className="recipe-image" src={recipe.imageUrl || 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600'} alt={recipe.title} >
                     </img>
@@ -63,27 +95,13 @@ const RecipePage = () => {
                     <h3>Ingredienser</h3>
                     <ul>
                         {recipe.ingredients.map((ingredient, index) => (
-                            <li key={index}>{ingredient.quantity + " " + ingredient.name}</li>
+                            <li key={index}><strong>{ingredient.quantity}</strong> <p>{ingredient.name}</p></li>
                         ))}
                     </ul>
                 </aside>
-
                 <div>
-                    <div className="recipe-header">
-                        <h1>{recipe.title}</h1>
-                        {isAuthenticated && <img src={editIcon} alt="Edit Recipe" className='icon' onClick={onEdit}/>}
-                        {isAuthenticated && <img src={deleteIcon} alt="Delete Recipe" className='icon delete-button' onClick={showDeleteModal}/>}
-
-                    </div>
-                    
-                    <div className="recipe-details">
-                        <p>{recipe.prepTime} min</p>
-                        <p>|</p>
-                        <p>{recipe.servings} portioner</p>
-                        <p>|</p>
-                        <p>{recipe.tags.map(tag => tag.name).join(", ")}</p>
-                    </div>
-
+                    {!isMobile && <Header />}
+                    <h3>Instruktioner</h3>
                     <ol>
                         {recipe.instructions.map((instruction, index) => (
                             <li key={index}>{instruction.text}</li>
