24790e4522eb4c189f34ff2b9fab969e63306ff2
Erik <erikpersson0884@gmail.com>
2026-03-26 15:59:50 +0100

    added github link to footer

--- client/src/layout/footer/Footer.tsx
+++ client/src/layout/footer/Footer.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Footer.css';
 
+import githubLogo from '../../assets/images/github.svg';
+
 const Footer = () => {
     return (
         <footer className="page-footer">
             <p>© 2025 <a href="https://github.com/adenholm">Hanna</a>s & <a href="https://github.com/erikpersson0884">Erik</a>s Recept</p>
+            
+            <a className="github-link" href="github.com/Adenholm/heap-recipes">
+                <img src={githubLogo} alt="GitHub Logo" height={35} />
+            </a>
         </footer>
     );
 }
